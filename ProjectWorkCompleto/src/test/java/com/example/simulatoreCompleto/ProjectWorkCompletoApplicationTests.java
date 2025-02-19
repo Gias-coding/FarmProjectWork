7590f4cea1afd42c7bd130d9072bdb9f4e516b71
Gias-coding <zinotto.gaspengo@gmail.com>
2025-02-19 09:25:18 +0100

    Add files via upload

---
+++ ProjectWorkCompleto/src/test/java/com/example/simulatoreCompleto/ProjectWorkCompletoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simulatoreCompleto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWorkCompletoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
